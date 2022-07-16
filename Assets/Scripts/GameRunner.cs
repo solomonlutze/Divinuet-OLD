99a9333e1e70bb7317b0215d285aa999b46eb283
Megan <meg.carnes@gmail.com>
2022-07-15 19:22:39 -0500

    made a new system for generative accompaniment keys and also added some queens

--- Assets/Scripts/GameRunner.cs
+++ Assets/Scripts/GameRunner.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4176fb02e21ba296df88ec11785bccda02519ee351df09b620a0e15f1eeae83d
-size 28870
+oid sha256:bbf7a561debde9fb1a73bdf4983ac6232e0ed882aae8e3cfee5b7c03409f5a83
+size 30209
--- Assets/Scripts/TarotCardData.cs
+++ Assets/Scripts/TarotCardData.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:628bf354769bf21c31aa3efbb21a8eb4ab3151df5a1a3c6f71430372031779be
-size 1816
+oid sha256:a15d297cab9596c15c801df26fb14d22eb17201f2dc31832f2dacdfbfed5dd8c
+size 1818
