8b294a965e9c3dadc4b7af42d6868e2d2aa85e9c
Megan <meg.carnes@gmail.com>
2022-07-18 13:34:11 -0500

    title cards update

--- Assets/Scripts/GameRunner.cs
+++ Assets/Scripts/GameRunner.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e4dbabbeb6ed995b1023b5b2673035bf1b51bf4755ee4540294a1e5e8c98b799
-size 30215
+oid sha256:35a9945bc6299196dadb7d75560ac59c22ea4f93a1fb58eb9be14bbc8de45d07
+size 30351
