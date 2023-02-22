6083f34970d222fde3a6df6d476df46e3a35b6b1
sol <solomon.lutze@gmail.com>
2023-02-22 12:37:13 -0500

    scrub shit
    fixed busted gamerunner script

--- Assets/Scripts/GameRunner.cs
+++ Assets/Scripts/GameRunner.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2451a33475141bdf015b6d8e2b95ccb0148eb74fe0b8dd217757e18c9ebc6956
-size 28751
+oid sha256:58461138ddd52d26783fd02f9e01b127df8654c519c07599f2262334ec2a0ee8
+size 30944
